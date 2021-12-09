3b6621697908075ddf52f5343ab65ac4c8320bb7
Daniel Rodrigues <danielaires95@gmail.com>
2021-12-09 08:39:23 -0300

    Construcao de api

---
+++ src/main/java/com/projeto/api/projetoapi/ProjetoapiApplication.java
@@ -0,0 +1,15 @@
+package com.projeto.api.projetoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjetoapiApplication.class, args);
+
+	}
+
+}
